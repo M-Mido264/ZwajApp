0777431928ead3394046be042cd88df00f4cc46e
M-Mido264 <mido_medhat1997@yahoo.com>
2019-08-27 18:46:58 -0700

    Initial Commit

---
+++ ZwajApp/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.Models;
+
+namespace ZwajApp.Data
+
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
